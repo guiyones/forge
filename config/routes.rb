82752c564ce4f2a2416d130b55a902a27a52e211
Guilherme <guiyonesnogara@gmail.com>
2026-03-11 15:05:50 -0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root "home#index"
+
+  resource :session
+  resource :registration, only: [:new, :create]
+  resources :passwords, param: :token
+
+  get "up" => "rails/health#show", as: :rails_health_check
+end
+
