72f6b846116217023de6e5b8f3d725486ccb57ce
Guilherme <guiyonesnogara@gmail.com>
2026-03-18 12:35:13 -0300

    fix: corrige erro de validação no primeiro checkin de novo desafio

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -11,8 +11,8 @@ class CheckinsController < ApplicationController
       return
     end
 
-    @checkin = @challenge.checkins.build(checkin_params)  
-    @challenge.update!(started_at: Time.current) if @challenge.started_at.nil?
+    @challenge.update_column(:started_at, Time.current) if @challenge.started_at.nil?
+    @checkin = @challenge.checkins.build(checkin_params)
     @checkin.day_number = (Date.current - @challenge.started_at.to_date).to_i + 1
 
     if @checkin.save
